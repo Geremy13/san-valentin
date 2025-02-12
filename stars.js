73fc23df0183632d85e16d11e31d3af50678c243
Geremy Heredia <100320755+Geremy13@users.noreply.github.com>
2025-02-11 19:30:45 -0500

    Add files via upload

---
+++ stars.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const starsContainer = document.querySelector('.stars');
+    const starCount = 100;
+
+    for (let i = 0; i < starCount; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.animationDelay = `${Math.random() * 1.5}s`;
+        starsContainer.appendChild(star);
+    }
+});
